5d741d2575a9d4d7317980e5f62f7d60cb10d061
Aster Haven (@therebelrobot) <asterhaven@pm.me>
2024-06-13 14:18:04 -0400

    updated stuff

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { Options } from './quartz/components/ExplorerNode';
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -10,6 +11,50 @@ export const sharedPageComponents: SharedLayout = {
       GitHub: "https://github.com/therebelrobot/portfolio",
     },
   }),
+}
+
+const YEAR_REGEX = /^[0-9]{4}/;
+const explorerOpts: Options = {
+  useSavedState: false,
+  order: ["sort", "map"],
+  folderClickBehavior: "link",
+  folderDefaultState: "collapsed",
+  title: "Pages",
+  sortFn: (a, b) => {
+
+    const aIsYear = YEAR_REGEX.test(a.name);
+    const bIsYear = YEAR_REGEX.test(b.name);
+  
+    if (aIsYear && bIsYear) {
+      return Number(b.name.slice(0, 4)) - Number(a.name.slice(0, 4));
+    }
+  
+    if (!aIsYear && !bIsYear) {
+      return a.name.localeCompare(b.name);
+    }
+  
+    return aIsYear ? -1 : 1;
+  },
+  mapFn: (node) => {
+    const colonArray = node.displayName.split(":").map((s) => s.trim());
+    node.displayName = colonArray[colonArray.length - 1].trim();
+
+    const originalName = colonArray.join(":").trim();
+
+    if (originalName.includes("Side Projects")) {
+      node.displayName = `🛠️ ${node.displayName}`;
+    }
+    if (originalName.includes("Work Experience") || YEAR_REGEX.test(originalName)) {
+      node.displayName = `💼 ${node.displayName}`;
+    }
+    if (originalName.includes("Blog")) {
+      node.displayName = `📝 ${node.displayName}`;
+    }
+    if (originalName.includes("Volunteer")) {
+      node.displayName = `🌱 ${node.displayName}`;
+    }
+  },
+  filterFn: (node) => node.name !== "tags",
 }
 
 // components for pages that display a single page (e.g. a single note)
@@ -25,9 +70,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer(explorerOpts)),
   ],
   right: [
+    Component.MobileOnly(Component.Explorer(explorerOpts)),
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
@@ -42,6 +88,12 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer(explorerOpts)),
   ],
-  right: [],
+  right: [
+    Component.MobileOnly(Component.Explorer(explorerOpts)),
+    Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
+  ],
 }
